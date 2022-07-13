65025a5fd39e8228d00890991c91f84d6bc98bcf
gdantimi <gianluigi.dantimi@dxc.com>
2022-07-13 16:58:25 +0200

    init header

--- react-flux-app/src/App.js
+++ react-flux-app/src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './component/header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-      </header>
+      <Header></Header>
     </div>
   );
 }
---
+++ react-flux-app/src/component/header/Header.js
@@ -0,0 +1,27 @@
+import './Header.css';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import logo from '../../logoRistorante-bianco.png';
+
+function Header() {
+    return (
+        <div >
+            <Navbar className='sfondoNav'>
+                <Container>
+                    <Navbar.Brand>
+                        <img
+                            alt=""
+                            src={logo}
+                            width="40"
+                            height="40"
+                            className="d-inline-block align-top"
+                        />
+                        <i className='titoloNav'>Ristorante</i>                        
+                    </Navbar.Brand>
+                </Container>
+            </Navbar>
+        </div>
+    );
+}
+
+export default Header;
