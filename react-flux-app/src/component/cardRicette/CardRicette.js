7571da4a4ce0000b55de52114f764204625b72e7
gdantimi <gianluigi.dantimi@dxc.com>
2022-07-14 18:10:12 +0200

    rinominata cardRicette

---
+++ react-flux-app/src/component/cardRicette/CardRicette.js
@@ -0,0 +1,20 @@
+import './CardRicette.css';
+
+function CardRicette(props) {
+    return (
+        <div className="col-md-4 col-sm-6 content-card">
+            <div className="card-big-shadow">
+                <div className="card card-just-text " data-background="color" data-color={props.colore} data-radius="none">
+                    <div className="content">
+                    <h6 class="category">{props.categoria}</h6>
+                        <h4 className="title">{props.titolo}</h4>
+                        <p className="description"> INGREDIENTI: {props.ingredienti}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
+export default CardRicette;
