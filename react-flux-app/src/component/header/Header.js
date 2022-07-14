23e79c35691bbf628bf0871ddc58108cd17e065b
gdantimi <gianluigi.dantimi@dxc.com>
2022-07-14 12:57:58 +0200

    update header

--- react-flux-app/src/component/header/Header.js
+++ react-flux-app/src/component/header/Header.js
@@ -1,23 +1,29 @@
 import './Header.css';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import logo from '../../logoRistorante-bianco.png';
 
 function Header() {
     return (
         <div >
-            <Navbar className='sfondoNav'>
+            <Navbar className='sfondoNav' variant="dark">
                 <Container>
-                    <Navbar.Brand>
+                    <Navbar.Brand href="/">
                         <img
-                            alt=""
                             src={logo}
                             width="40"
                             height="40"
                             className="d-inline-block align-top"
                         />
-                        <i className='titoloNav'>Ristorante</i>
+                        <i className='titoloNav'>Ricettario</i>
                     </Navbar.Brand>
+
+                    <Nav className="me-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/ricette">Ricette</Nav.Link>
+                        <Nav.Link href="/informazioni">Informazioni</Nav.Link>
+                    </Nav>
                 </Container>
             </Navbar>
         </div>
