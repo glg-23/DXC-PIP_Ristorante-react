04aedb5f4f0c8e4c9d29fb84c95918b1022d70e4
gdantimi <gianluigi.dantimi@dxc.com>
2022-07-14 13:01:09 +0200

    init pagina non trovata

---
+++ react-flux-app/src/component/noPage/NoPage.js
@@ -0,0 +1,12 @@
+import './NoPage.css';
+import { Button } from 'bootstrap';
+
+function NoPage() {
+    return (
+        <div>
+            <h1>La pagina non è stata trovata</h1>
+        </div>
+    );
+}
+
+export default NoPage;
