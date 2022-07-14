c105f54f9909cd30264c2a2e3590ab24965ba9f1
gdantimi <gianluigi.dantimi@dxc.com>
2022-07-14 13:01:35 +0200

    init ricette

---
+++ react-flux-app/src/component/ricette/Ricette.js
@@ -0,0 +1,12 @@
+import './Ricette.css';
+import { Button } from 'bootstrap';
+
+function Ricette() {
+    return (
+        <div>
+            <h1>Elenco ricette</h1>
+        </div>
+    );
+}
+
+export default Ricette;
