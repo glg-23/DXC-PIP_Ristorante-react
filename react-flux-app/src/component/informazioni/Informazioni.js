636700cbedc8e0b259ddb98dcce2c2d551a775a4
gdantimi <gianluigi.dantimi@dxc.com>
2022-07-14 12:59:41 +0200

    init informazioni

---
+++ react-flux-app/src/component/informazioni/Informazioni.js
@@ -0,0 +1,11 @@
+import './Informazioni.css';
+
+function Informazioni() {
+    return (
+        <div >
+            <h2> Qui potrai trovare l'elenco delle ricette ed inserirne di nuove</h2>
+        </div>
+    );
+}
+
+export default Informazioni;
