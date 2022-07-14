05fd592d1aea6646d62cb18d2cfe47d1acaae15a
gdantimi <gianluigi.dantimi@dxc.com>
2022-07-14 12:54:10 +0200

    init button

---
+++ react-flux-app/src/component/button/Button.js
@@ -0,0 +1,16 @@
+import './Button.css';
+import Button from 'react-bootstrap/Button';
+
+function Button() {
+    return (
+        <div >
+            <Button
+                size="lg"
+                className='button'
+                onClick={''}
+            >{''}</Button>
+        </div>
+    );
+}
+
+export default Button;
