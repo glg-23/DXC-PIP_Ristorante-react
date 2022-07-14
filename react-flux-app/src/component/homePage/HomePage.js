798a7feb9a9a0fecb80f04aa5b4d24b37356318a
gdantimi <gianluigi.dantimi@dxc.com>
2022-07-14 18:08:22 +0200

    update homePage

--- react-flux-app/src/component/homePage/HomePage.js
+++ react-flux-app/src/component/homePage/HomePage.js
@@ -1,33 +1,11 @@
 import './HomePage.css';
-import CardHomePage from '../cardHomePage/CardHomePage';
 
 function HomePage() {
     return (
         <div>
             <h1>Benvenuto</h1>
-
-            <div className='row'>
-                <CardHomePage
-                    titolo='Ricette'
-                    testo="Clicca qui per visualizzare l'elenco delle ricette"
-                    collegamento='/ricette'
-                    colore='blue'>
-                    </CardHomePage>
-
-                <CardHomePage
-                    titolo='Informazioni'
-                    testo="Clicca qui per avere maggiori informazioni"
-                    collegamento='/informazioni'
-                    colore='yellow'>
-                    </CardHomePage>
-
-                <CardHomePage
-                    titolo='#######'
-                    testo=""
-                    collegamento='/'
-                    colore='green'>
-                    </CardHomePage>
-            </div>
+            <br/>
+            <h3>In questo sito puoi visualizzare i piatti nel menù ed inserirne di nuovi</h3>
         </div >
     );
 }
