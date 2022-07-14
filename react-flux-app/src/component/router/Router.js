b69a651490b3b7f8f52d0cfa12e6654f35d783e9
gdantimi <gianluigi.dantimi@dxc.com>
2022-07-14 12:52:53 +0200

    init router

---
+++ react-flux-app/src/component/router/Router.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from '../homePage/HomePage';
+import Ricette from '../ricette/Ricette';
+import Informazioni from '../informazioni/Informazioni';
+import NoPage from '../noPage/NoPage';
+
+
+function Router() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<Home />} />
+                <Route path="Ricette" element={<Ricette />} />
+                <Route path="informazioni" element={<Informazioni />} />
+                <Route path="*" element={<NoPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default Router;
