96b0e5f807422c4e68e1b185e137ee1cfce4ceb9
gdantimi <gianluigi.dantimi@dxc.com>
2022-07-14 18:10:28 +0200

    fix layout informazioni

--- react-flux-app/src/component/informazioni/Informazioni.js
+++ react-flux-app/src/component/informazioni/Informazioni.js
@@ -4,11 +4,12 @@ import BottoneHome from '../bottoneHome/BottoneHome';
 function Informazioni() {
     return (
         <div>
-            <h2> In questo sito potrai trovare l'elenco delle ricette ed inserirne di nuove</h2>
-
             <div className='contenitoreFirma'>
-                <p> DXC Performance Improvement Plan - Corso react 07/2022 </p>
-                <p> Gianluigi D'Antimi</p>
+                <h3> DXC Performance Improvement Plan </h3>
+                <br />
+                <h4> Corso react - 07/2022 </h4>
+                <br />
+                <h5> Gianluigi D'Antimi </h5>
             </div>
 
             <BottoneHome />
