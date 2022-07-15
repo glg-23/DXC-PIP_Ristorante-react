14f55306332a9f74698add15a03951ea91101a42
gdantimi <gianluigi.dantimi@dxc.com>
2022-07-15 12:52:00 +0200

    refactor form aggiungiPiatto

--- react-flux-app/src/component/aggiungiPiatto/AggiungiPiatto.js
+++ react-flux-app/src/component/aggiungiPiatto/AggiungiPiatto.js
@@ -1,45 +1,63 @@
 import './AggiungiPiatto.css';
-import Bottone from '../bottone/Bottone';
+import React from 'react';
 import Form from 'react-bootstrap/Form';
 
+class AggiungiPiatto extends React.Component {
+    state = {
+        categoria: "",
+        nomePiatto: "",
+        ingredienti: ""
+    }
 
-function AggiungiPiatto() {
-    return (
-        <div>
+    handleChange = (event) => {
+        const value = event.target.value;
+        const id = event.target.id;
+        this.setState({
+            [id]: value,
+        });
+        console.log(this.state)
+    }
 
-            <h2>Aggiungi un nuovo piatto</h2>
+    handleSubmit = (event) => {
+        alert('Piatto inserito: ' + this.state.nomePiatto);
+        event.preventDefault();
+    }
 
-            <form>
-                <Form.Group className="form-group" controlId="selectCategoria">
-                    <Form.Label>Categoria</Form.Label>
-                    <Form.Select>
-                        <option></option>
-                        <option>Antipasti</option>
-                        <option>Primi</option>
-                        <option>Secondi</option>
-                        <option>Contorni</option>
-                        <option>Dolci</option>
-                    </Form.Select>
-                </Form.Group>
+    render() {
+        return (
+            <div>
 
-                <Form.Group className="form-group" controlId="inputNome">
-                    <Form.Label>Nome del piatto</Form.Label>
-                    <Form.Control type="text" />
-                </Form.Group>
+                <h2>Aggiungi un nuovo piatto</h2>
 
-                <Form.Group className="form-group" controlId="exampleForm.ControlTextarea1">
-                    <Form.Label>Ingredienti</Form.Label>
-                    <Form.Control as="textarea" />
-                </Form.Group>
-            </form>
+                <form onSubmit={this.handleSubmit}>
+                    <Form.Group className="form-group">
+                        <Form.Label>Categoria</Form.Label>
+                        <Form.Select id="categoria" value={this.state.categoria} onChange={this.handleChange}>
+                            <option value=""></option>
+                            <option value="Antipasti">Antipasti</option>
+                            <option value="Primi">Primi</option>
+                            <option value="Secondi">Secondi</option>
+                            <option value="Contorni">Contorni</option>
+                            <option value="Dolci">Dolci</option>
+                        </Form.Select>
+                    </Form.Group>
 
-            <Bottone
-                testo='Aggiungi piatto'
-                linkBottone=''>
-            </Bottone>
+                    <Form.Group className="form-group" >
+                        <Form.Label>Nome del piatto</Form.Label>
+                        <Form.Control type="text" id="nomePiatto" value={this.state.nomePiatto} onChange={this.handleChange} />
+                    </Form.Group>
 
-        </div>
-    );
+                    <Form.Group className="form-group" >
+                        <Form.Label>Ingredienti</Form.Label>
+                        <Form.Control as="textarea" id="ingredienti" value={this.state.ingredienti} onChange={this.handleChange} />
+                    </Form.Group>
+
+                    <input type="submit" value="Submit" disabled={this.state.categoria === "" || this.state.nomePiatto === "" || this.state.ingredienti === ""} className="btn btn-group-lg btn-primary btn-lg bottoneSubmit" />
+                </form>
+
+            </div>
+        );
+    }
 }
 
 export default AggiungiPiatto;
