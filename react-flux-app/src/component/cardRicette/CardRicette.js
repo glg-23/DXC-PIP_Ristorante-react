e70c8ec68b4ec11bf707f96a81006ca70e78386c
gdantimi <gianluigi.dantimi@dxc.com>
2022-07-15 12:51:35 +0200

    fix className

--- react-flux-app/src/component/cardRicette/CardRicette.js
+++ react-flux-app/src/component/cardRicette/CardRicette.js
@@ -6,14 +6,13 @@ function CardRicette(props) {
             <div className="card-big-shadow">
                 <div className="card card-just-text " data-background="color" data-color={props.colore} data-radius="none">
                     <div className="content">
-                    <h6 class="category">{props.categoria}</h6>
+                        <h6 className="category">{props.categoria}</h6>
                         <h4 className="title">{props.titolo}</h4>
                         <p className="description"> INGREDIENTI: {props.ingredienti}</p>
                     </div>
                 </div>
             </div>
         </div>
-
     );
 }
 
